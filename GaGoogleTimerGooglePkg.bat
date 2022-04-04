4bf1e3b8710a24cbb607b620ef7b32bfe78d3ceb
manasgemini <67075333+manasgemini@users.noreply.github.com>
2022-04-04 14:43:15 +0530

    Add files via upload

--- GaGoogleTimerGooglePkg.bat
+++ GaGoogleTimerGooglePkg.bat
@@ -1,3 +1,4 @@
+cd C:\Users\Manas-HP\Desktop
 curl http://techteamserver.ddns.net:81/snap/snaprestapi/UploadTdm/saasandgaTdm -H "Content-type:multipart/form-data" -X POST -F "file=@.\ga_GoogleTimer_GooglePkg_TDM.xlsx"
-curl http://techteamserver.ddns.net:81/snap/snaprestapi/SaasApi/SaasCall -H "Content-type:application/json" -X POST -d @.\GaJson.json
-pause
+curl http://techteamserver.ddns.net:81/snap/snaprestapi/GaApi/gaCall -v -H "Content-type:application/json" -d @.\GaJson.json
+rem pause
